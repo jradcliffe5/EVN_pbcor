7b804fb0f07ff6210590e6024aa02c2bbd66241e
Jack Radcliffe <jackradcliffe@Jacks-MacBook-Pro.local>
2017-06-19 12:23:42 +0200

    changes

--- pbcor_model.py
+++ pbcor_model.py
@@ -16,7 +16,9 @@ import scipy.ndimage as ndimage
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from scipy.interpolate import interp2d
 from matplotlib.colors import LogNorm
+import pickle
 ## Set up observation information
+plt.ioff()
 
 def station_HPBW(station,frequency):
     HPBW = ((constants.c/frequency)/station)*(180/np.pi)
@@ -40,6 +42,7 @@ def station_table():
   stations["ONSALA60"] = 20/1.05
   stations["ONSALA85"] = 25/1.05
   stations["ONS_DBBC"] = stations["ONSALA85"]
+  stations["ROBLEDO"] = 70/1.05
   stations["SHANGHAI"] = 22.5 # 25
   stations["SVETLOE"] = 32/1.05
   stations["TORUN"] = 32/1.05
@@ -50,62 +53,131 @@ def station_table():
   stations["GBT_VLBA"] = 100. / 1.05
   return stations
 
-def generate_psf(phase_centers,telescope,model,scale):
+def generate_psf(phase_centers,telescope,model,scale): ##generate voltage beams!
     if model == 'gaussian':
-        for i in range(len(phase_centers)):
+        for i in range(len(phase_centers)): #gaussian of form
             if i == 0:
-                EF = models.Gaussian2D(amplitude=scale[i], x_mean=phase_centers[i].ra*u.degree, y_mean=phase_centers[i].dec*u.degree, x_stddev=(station_HPBW(stations[telescope],frequency)/(2*np.sqrt(2*np.log(2))))*u.degree*(1/np.cos(1.08)),y_stddev=(station_HPBW(stations[telescope],frequency)/(2*np.sqrt(2*np.log(2))))*u.degree,theta=0)
+                EF = models.Gaussian2D(amplitude=scale[i], \
+                x_mean=phase_centers[i].ra*u.degree,\
+                y_mean=phase_centers[i].dec*u.degree,\
+                x_stddev=(station_HPBW(stations[telescope],frequency)/(2*np.sqrt(np.log(2))))*u.degree*(1/np.cos((phase_centers[i].dec.radian))),\
+                y_stddev=(station_HPBW(stations[telescope],frequency)/(2*np.sqrt(np.log(2))))*u.degree,theta=0)
             else:
-                EF = EF + models.Gaussian2D(amplitude=scale[i], x_mean=phase_centers[i].ra*u.degree, y_mean=phase_centers[i].dec*u.degree, x_stddev=(station_HPBW(stations[telescope],frequency)/(2*np.sqrt(2*np.log(2))))*u.degree*(1/np.cos(1.08)),y_stddev=(station_HPBW(stations[telescope],frequency)/(2*np.sqrt(2*np.log(2))))*u.degree,theta=0)
+                EF = EF + models.Gaussian2D(amplitude=scale[i], x_mean=phase_centers[i].ra*u.degree, y_mean=phase_centers[i].dec*u.degree, x_stddev=(station_HPBW(stations[telescope],frequency)/(2*np.sqrt(np.log(2))))*u.degree*(1/np.cos(phase_centers[i].dec.radian)),y_stddev=(station_HPBW(stations[telescope],frequency)/(2*np.sqrt(np.log(2))))*u.degree,theta=0)
     else:
         print 'Gaussians only please'
         sys.exit()
     return EF
 
-def convertAIPStoPythonImage(filename,outfilename):
-    hdu_list = fits.open(filename)
-
-    head = hdu_list['PRIMARY'].header
-    head['CRVAL1'] = 360 - (head['CRVAL1']*-1)
-    head['NAXIS'] = 2
-    del head['NAXIS3']
-    del head['NAXIS4']
-    del head['CTYPE3'],  head['CRVAL3'], head['CDELT3'], head['CRPIX3'], head['CROTA3']
-    del head['CTYPE4'], head['CRVAL4'], head['CDELT4'], head['CRPIX4'], head['CROTA4']
-    #if filename.endswith('NA.fits') == False:
-    #head.set('BMIN', float(hdu_list[1].data.field('BMIN')))
-    #head.set('BMAJ', float(hdu_list[1].data.field('BMAJ')))
-    #head.set('BPA', float(hdu_list[1].data.field('BPA')))
-    image_data = hdu_list[0].data
-    image_data = image_data[0,0,:,:]
-    hdu = fits.PrimaryHDU(image_data, header=head)
-    hdulist = fits.HDUList([hdu])
-    hdulist.writeto(outfilename,clobber=True)
-    return outfilename
-
-
+#######################
+######## Inputs #######
+#######################
 # Set up coordinate systems for observation
-c = SkyCoord(ra=['12h37m20s','12h36m20s','12h36m20s','12h37m20s','12h36m50s'],dec=['+62d16m28s','+62d16m28s','+62d09m28s','+62d09m28s','+62d12m58s'], frame = 'icrs')
-scales = [1,1,1,1,1./16.]
-c1 = SkyCoord(ra=['12h37m20s'],dec=['+62d16m28s'])
+### Multiple pointings ###
+Positions1_RA = ['12h37m20s','12h36m20s','12h36m20s','12h37m20s','12h36m50s']
+Positions1_Dec = ['+62d16m28s','+62d16m28s','+62d09m28s','+62d09m28s','+62d12m58s']
+scales = [1,1,1,1,1/16] ## relative observing time propto sqrt(time_per_pointing)
+#--
+### Central pointings ###
+Positions2_RA = ['12h36m50s']
+Positions2_Dec = ['+62d12m58s']
+
+### central frequency of observations
+frequency = 1.65849E9
+### telescopes participating (in order of AIPS antenna table!)
+telescopes = ['EFLSBERG','WSTRBORK','ROBLEDO','ONSALA60',"MEDICINA",'NOTO',"TORUN",\
+              'SVETLOE',"BADARY","ZELENCHK","URUMQI","SHANGHAI","JODRELL1"]
+outside_telescopes = ['EFLSBERG','JODRELL1']
+
+#### Path to fitsfiles to extract corrdinates in EG078.npy is not here
+path='/net/10.0.6.249/volume1/data/radcliff/EG078B/MSSC_PBCOR/wrong_model_MSSC/Tapered_weights/'
+#######################
+##-------------------##
+#######################
+
+## Generate skycoordinates of outside (c) and central (c1)
+# outside pointings
+c = SkyCoord(ra=Positions1_RA,\
+            dec=Positions1_Dec, frame = 'icrs',unit=('hour','deg'))
+# central pointings
+c1 = SkyCoord(ra=Positions2_RA,dec=Positions2_Dec,frame ='icrs',unit=('hour','deg'))
 print c.ra*u.degree
 print c.dec*u.degree
+
+
 ## Set up beams as Gaussians for all positions
 stations = station_table()
 
-## Set up variables for the observations
-
-frequency = 1.6E9
-telescopes = ['EFLSBERG','JODRELL1','ONSALA60',"SHANGHAI","BADARY",'NOTO','SVETLOE']
+## Generate telescope single voltage patterns with sky projection included.
 telescope_single_psf = {}
 for i in range(len(telescopes)):
-    telescope_single_psf[telescopes[i]] = generate_psf(c,telescopes[i],'gaussian',scales)
+    if telescopes[i] in outside_telescopes:
+        telescope_single_psf[telescopes[i]] = generate_psf(c,telescopes[i],'gaussian',scales)
+    else:
+        telescope_single_psf[telescopes[i]] = generate_psf(c1,telescopes[i],'gaussian',scales)
 
-print telescope_single_psf.values()
 
+## Set the positions of observations
+RA= []
+DEC = []
+filenames = []
+
+if os.path.exists('./EG078B.npy'):
+    RA = np.load('./EG078B.npy')[1]
+    DEC = np.load('./EG078B.npy')[2]
+    filenames = np.load('./EG078B.npy')[0]
+else:
+    for file in os.listdir(path):
+        if file.endswith('.fits'):
+            hdu = fits.open(path+file)
+            print file
+            #print 360+hdu[0].header['CRVAL1']
+            RA.append(360+float(hdu[0].header['CRVAL1']))
+            DEC.append(float(hdu[0].header['CRVAL2']))
+            filenames.append(file[:8])
+            np.save('EG078B.npy',[filenames,RA,DEC])
+
+## plot telescope_single_psfs
+import pandas as pd
+detections = pd.read_csv('VLBI_Catalogue_v10.csv',delimiter='\t')
+print detections.columns
+detections = SkyCoord(detections.VLBI_RA,detections.VLBI_Dec,frame='icrs',unit=('hour','deg'))
+for i in range(len(telescopes)):
+    x, y = np.mgrid[188.3:190.3:500j,61.7:62.7:500j]
+    plot = telescope_single_psf[telescopes[i]]
+    EG078B_positions = SkyCoord(RA,DEC,unit='deg',frame='icrs')
+    plt.figure(i,figsize=(8, 8))
+    plt.pcolormesh(x,y,plot(x,y)/np.max(plot(x,y)),cmap='magma',vmin=0.4,vmax=0.6)
+    #plt.colorbar()
+    plt.scatter(detections.ra,detections.dec)
+    plt.gca().invert_xaxis()
+    plt.grid(color='w')
+    plt.savefig(telescopes[i]+'_single_primary_beam.png',bbox_inches='tight',)
+    plt.close('all')
+
+## Generate voltage beam corrections for CLCOR
+EG078B_CLCOR_corr = []
+
+for i in range(len(filenames)):
+    single_psf_corr = []
+    for j in range(len(telescopes)):
+        x, y = np.mgrid[188.5:190:500j,61.9:62.6:500j]
+        single_psf_corr = single_psf_corr + [1/(telescope_single_psf[telescopes[j]](RA[i],DEC[i])/np.max(telescope_single_psf[telescopes[j]](x,y)))]
+        print telescopes[j]
+        print 1/(telescope_single_psf[telescopes[j]](RA[i],DEC[i])/np.max(telescope_single_psf[telescopes[j]](x,y)))
+    EG078B_CLCOR_corr = EG078B_CLCOR_corr + [[filenames[i],RA[i],DEC[i],single_psf_corr]]
+
+os.system('rm CLCOR_params.pckl')
+f = open('CLCOR_params.pckl', 'wb')
+pickle.dump(EG078B_CLCOR_corr, f)
+f.close()
+
+'''
+##Generate baseline pair voltage beams
 telescope_baseline_pairs = {}
 key = telescope_single_psf.keys()
 values = telescope_single_psf.values()
+
 for i in range(len(values)):
     for j in range(len(values)):
         if key[i] != key[j]:
@@ -117,6 +189,7 @@ for i in range(len(values)):
             print key[i]+' is the same as '+key[j]
 print telescope_baseline_pairs.keys()
 
+#combine baseline beams to form primary beam
 plot= []
 for i in range(len(telescope_baseline_pairs.values())):
     if i == 0:
@@ -125,31 +198,23 @@ for i in range(len(telescope_baseline_pairs.values())):
     else:
         plot = plot+telescope_baseline_pairs.values()[i]
 
+
+## Plots for combining the arrays
 fig = plt.figure(1)
-RA= []
-DEC = []
-
-## Set the positions of observations
-if os.path.exists('./coordinatesDEC.npy') and os.path.exists('./coordinatesRA.npy'):
-    RA = np.load('coordinatesRA.npy')
-    DEC = np.load('coordinatesDEC.npy')
-else:
-    for file in os.listdir('./FITSImages/'):
-        if file.endswith('.fits'):
-            hdu = fits.open('FITSImages/'+file)
-            print file
-            #print 360+hdu[0].header['CRVAL1']
-            RA.append(360+float(hdu[0].header['CRVAL1']))
-            DEC.append(float(hdu[0].header['CRVAL2']))
-            image_data = hdu[0].data
-            image_data = image_data[0,0,:,:]
-            np.save('coordinatesRA.npy',RA)
-            np.save('coordinatesDEC.npy',DEC)
-
 x, y = np.mgrid[188.5:190:500j,61.9:62.6:500j]
 c1 = SkyCoord(RA,DEC,unit='deg',frame='icrs')
+plt.figure(1)
 plt.pcolormesh(x,y,plot(x,y)/np.max(plot(x,y)),cmap='magma',norm=LogNorm())
 plt.colorbar()
 plt.scatter(c1.ra,c1.dec)
 plt.gca().invert_xaxis()
 plt.show()
+
+## Pwer beam plot
+plt.figure(2)
+plt.pcolormesh(x,y,(plot(x,y)/np.max(plot(x,y)))**2,cmap='viridis')
+plt.colorbar()
+plt.scatter(c1.ra,c1.dec)
+plt.gca().invert_xaxis()
+plt.show()
+'''
