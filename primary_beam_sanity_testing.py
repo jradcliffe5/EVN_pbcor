1bd05e42495f4cb06760ad954cb598fa670d9a4d
jradcliffe5 <jack.radcliffe@postgrad.manchester.ac.uk>
2017-08-23 19:42:49 +0200

    updated pb checks

--- primary_beam_sanity_testing.py
+++ primary_beam_sanity_testing.py
@@ -21,12 +21,29 @@ from time import gmtime, strftime, localtime
 ti = time.time()    # To time the script
 
 #### Inputs ####
-AIPS.userno = 10
-uvdata = AIPSUVData('HDFC0214','UV',1,1)
-flagfiles = os.listdir('./flag_tables')
+AIPS.userno = 11
+uvdata = AIPSUVData('HDFC0214','MULTI',1,1)
+path = '/net/10.0.6.249/volume1/data/radcliff/EG078B/MSSC_PBCOR/Correct_model_multiple_pointings/PB_testing/'
 ################
 
-x = flagfiles
-for i in flagfiles:
-    while i in x: x.remove(i)
-        print x
+for i in os.listdir('./flag_tables'):
+    x = os.listdir('./flag_tables')
+    uvdata.zap_table('FG',1)
+    while i in x:
+        x.remove(i)
+    for j in x:
+        uvflg = AIPSTask('UVFLG')
+        uvflg.indata = uvdata
+        uvflg.opcode = 'FLAG'
+        uvflg.intext = '%sflag_tables/%s' % (path,j)
+        uvflg.go()
+    imagr = AIPSTask('IMAGR')
+    imagr.indata = uvdata
+    imagr.sources[1] = 'HDFC0214'
+    imagr.docalib = 2
+    imagr.cellsize[1:] = 0.001,0.001
+    imagr.imsize[1:] = 512, 512
+    imagr.nchav = 32
+    imagr.outname = i
+    imagr.niter = 300
+    imagr.go()
